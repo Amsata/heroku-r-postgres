2ff0a71e34366af455295e93f4ac14d01ff18acb
Amsata NIANG <amsata_niang@yahoo.fr>
2021-08-20 17:05:33 +0200

    update

--- init.R
+++ init.R
@@ -1,9 +1,9 @@
-my_packages=c("RPostgreSQL","RPostgres", "httr","shiny","dplyr","tidyr")
+helpers.installPackages=c("RPostgreSQL","RPostgres", "httr","shiny","dplyr","tidyr")
 
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
-  }
-}
-
-invisible(sapply(my_packages, install_if_missing))
+# install_if_missing = function(p) {
+#   if (p %in% rownames(installed.packages()) == FALSE) {
+#     install.packages(p)
+#   }
+# }
+# 
+# invisible(sapply(my_packages, install_if_missing))
