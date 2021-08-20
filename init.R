8d296ff92b6cc986658f0c4a19366c28686a36d9
Amsata NIANG <amsata_niang@yahoo.fr>
2021-08-20 18:57:10 +0200

    update

--- init.R
+++ init.R
@@ -1,11 +1,11 @@
 options(encoding = "UTF-8")
 
-helpers.installPackages("RPostgreSQL","RPostgres", "httr","shiny","dplyr","tidyr")
+my_packages=c("RPostgreSQL","RPostgres", "httr","shiny","dplyr","tidyr")
 
-# install_if_missing = function(p) {
-#   if (p %in% rownames(installed.packages()) == FALSE) {
-#     install.packages(p)
-#   }
-# }
-# 
-# invisible(sapply(my_packages, install_if_missing))
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
