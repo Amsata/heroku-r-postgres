3a160a32c64a3e0e9ddcb64cadcc946ffb6c6ad0
Amsata NIANG <amsata_niang@yahoo.fr>
2021-08-20 16:15:48 +0200

    update

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ shinyServer(function(input, output) {
     library("httr")
     library("RPostgreSQL")
     library("RPostgres")
-    db_uri <- Sys.getenv('DATABASE_URL')
+    # db_uri <- Sys.getenv('DATABASE_URL')
     db_uri="postgres://xfaefhqgdtlcle:82ec6a4179aef9d3c11dbdc8cae8c0a78f258308c4a3844835330dfd81eb189b@ec2-52-6-211-59.compute-1.amazonaws.com:5432/d1qcu3g4pf06t5"
     parts <- parse_url(db_uri)
     
